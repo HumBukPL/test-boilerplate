3fa9f726dad23c62e5d1effbfb43d824931e3abf
Karol Kołodziejczyk <kkolodziejczyk@codeton.local>
2022-06-30 14:12:04 +0200

    naprawiam se

--- server/subgraphs/models/user.js
+++ server/subgraphs/models/user.js
@@ -1,11 +1,8 @@
 import mongoose from "mongoose";
-<<<<<<< HEAD
+import Task from './task'
 import bcrypt from "bcrypt";
 
 const { SECRET_KEY } = process.env
-=======
->>>>>>> d4d33774232ff44d561bc156134c96e3bc09c519
-import Task from './task'
 
 const UserSchema = new mongoose.Schema({
   login:
