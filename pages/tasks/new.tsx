46a52f52763432d1ed31b440de0ce63937cf617b
Jakub Znyk <jznyk@codeton.local>
2022-06-30 09:30:41 +0200

    Improved new task form

--- pages/tasks/new.tsx
+++ pages/tasks/new.tsx
@@ -4,9 +4,13 @@ import NewTaskForm from '../../views/components/tasks/NewTaskForm'
 import Card from '@views/components/ui/Card'
 
 const NewTask = () => {
+  const addTaskHandler = (obj : object) => {
+    //TODO: Wyslac zapytanie do backendu z dodaniem taska
+  }
+
   return (
     <section>
-      <NewTaskForm />
+      <NewTaskForm onAddTask={addTaskHandler}/>
     </section>
   )
 }
--- views/components/tasks/NewTaskForm.tsx
+++ views/components/tasks/NewTaskForm.tsx
@@ -1,18 +1,38 @@
 import React from 'react'
+import { useRef, useState } from 'react'
+import router, { useRouter } from 'next/router'
 
 import classes from './NewTaskForm.module.css'
 
 const NewTaskForm = (props: any) => {
+  const [sendingTask, setSendingTask] = useState(false)
+  const titleInputRef = useRef()
+  const descInputRef = useRef()
+
+  const onSubmitForm = (e: any) => {
+    e.preventDefault()
+    setSendingTask(true)
+
+    const enteredTitle = titleInputRef.current.value
+    const enteredDesc = descInputRef.current.value
+
+    //TODO: Wyslac obiekt
+    props.onAddTask({})
+
+    router.push('/tasks')
+  }
+
+  if (sendingTask) return <h1>Saving task</h1>
   return (
     <div className={classes.newtask}>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={onSubmitForm}>
         <div className={classes.title}>
           <label htmlFor="title">Tytuł:</label>
-          <input type="text" />
+          <input type="text" ref={titleInputRef} required />
         </div>
         <div className={classes.field}>
           <label htmlFor="desc">Opis:</label>
-          <textarea className={classes.ta}/>
+          <textarea className={classes.ta} ref={descInputRef} required />
         </div>
         <div className={classes.field}>
           <button className={classes.save__btn}>ZAPISZ TASKA</button>
