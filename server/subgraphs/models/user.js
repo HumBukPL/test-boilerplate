f97d22b8d9efd0c18809ca2c19a65cec86ced4ca
m5bunny <yurii.melnychuk.contact@gmail.com>
2022-06-30 14:14:07 +0200

    merge

--- server/subgraphs/models/user.js
+++ server/subgraphs/models/user.js
@@ -3,8 +3,6 @@ import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 
 const { SECRET_KEY } = process.env
-=======
->>>>>>> d4d33774232ff44d561bc156134c96e3bc09c519
 import Task from './task'
 
 const UserSchema = new mongoose.Schema({
