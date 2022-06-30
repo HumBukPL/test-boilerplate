89d5eda2ec6e95bee0abe591a0382edef8a1c9a0
Jakub Znyk <jznyk@codeton.local>
2022-06-30 14:20:24 +0200

    minor changes

--- pages/tasks/new.tsx
+++ pages/tasks/new.tsx
@@ -4,6 +4,21 @@ import { gql, useMutation } from '@apollo/client'
 import NewTaskForm from '../../views/components/tasks/NewTaskForm'
 import Card from '@views/components/ui/Card'
 
+
+const CREATE_LINK_MUTATION = gql`
+  mutation taskCreateOne(
+    $title: String!
+    $description: String!
+  ) {
+    post(title: $title, description: $description) {
+      id
+      createdAt
+      url
+      description
+    }
+  }
+`;
+
 const NewTask = () => {
   const addTaskHandler = (obj : object) => {
     //TODO: Wyslac zapytanie do backendu z dodaniem taska
